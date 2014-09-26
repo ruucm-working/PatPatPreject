d5d495221d6b5e777480244088bee041693e82bd
ruucm <bumbermin@naver.com>
2014-09-27 04:11:19 +0900

    add Bouncing Anim

--- Mario/src/com/exam/view/bluetooth/BluetoothChat.java
+++ Mario/src/com/exam/view/bluetooth/BluetoothChat.java
@@ -16,11 +16,20 @@
 
 package com.exam.view.bluetooth;
 
+import java.util.ArrayList;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -29,17 +38,29 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.exam.R;
+import com.exam.helper.ShapeHolder;
+import com.exam.view.bluetooth.BouncingBalls.MyAnimationView;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorListenerAdapter;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ArgbEvaluator;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.ValueAnimator;
 
 /**
  * This is the main Activity that displays the current chat session.
@@ -88,7 +109,19 @@ public class BluetoothChat extends Activity {
         if(D) Log.e(TAG, "+++ ON CREATE +++");
 
         // Set up the window layout
-        setContentView(R.layout.main);
+        setContentView(R.layout.battlezone_main);
+        
+        
+        
+        
+        
+        //Bouncingball anim
+        
+        LinearLayout container = (LinearLayout) findViewById(R.id.container);
+        container.addView(new MyAnimationView(this));
+        
+        
+        
 
         // Get local Bluetooth adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -355,5 +388,140 @@ public class BluetoothChat extends Activity {
         }
         return false;
     }
+    
+    
+    
+    public class MyAnimationView extends View {
+
+        private static final int RED = 0xffFF8080;
+        private static final int BLUE = 0xff8080FF;
+        private static final int CYAN = 0xff80ffff;
+        private static final int GREEN = 0xff80ff80;
+
+        public final ArrayList<ShapeHolder> balls = new ArrayList<ShapeHolder>();
+        AnimatorSet animation = null;
+
+        public MyAnimationView(Context context) {
+            super(context);
+
+            // Animate background color
+            // Note that setting the background color will automatically invalidate the
+            // view, so that the animated color, and the bouncing balls, get redisplayed on
+            // every frame of the animation.
+            ValueAnimator colorAnim = ObjectAnimator.ofInt(this, "backgroundColor", RED, BLUE);
+            colorAnim.setDuration(3000);
+            colorAnim.setEvaluator(new ArgbEvaluator());
+            colorAnim.setRepeatCount(ValueAnimator.INFINITE);
+            colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+            colorAnim.start();
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if (event.getAction() != MotionEvent.ACTION_DOWN &&
+                    event.getAction() != MotionEvent.ACTION_MOVE) {
+                return false;
+            }
+            ShapeHolder newBall = addBall(event.getX(), event.getY());
+
+            // Bouncing animation with squash and stretch
+            float startY = newBall.getY();
+            float endY = getHeight() - 50f;
+            float h = (float)getHeight();
+            float eventY = event.getY();
+            int duration = (int)(500 * ((h - eventY)/h));
+            ValueAnimator bounceAnim = ObjectAnimator.ofFloat(newBall, "y", startY, endY);
+            bounceAnim.setDuration(duration);
+            bounceAnim.setInterpolator(new AccelerateInterpolator());
+            ValueAnimator squashAnim1 = ObjectAnimator.ofFloat(newBall, "x", newBall.getX(),
+                    newBall.getX() - 25f);
+            squashAnim1.setDuration(duration/4);
+            squashAnim1.setRepeatCount(1);
+            squashAnim1.setRepeatMode(ValueAnimator.REVERSE);
+            squashAnim1.setInterpolator(new DecelerateInterpolator());
+            ValueAnimator squashAnim2 = ObjectAnimator.ofFloat(newBall, "width", newBall.getWidth(),
+                    newBall.getWidth() + 50);
+            squashAnim2.setDuration(duration/4);
+            squashAnim2.setRepeatCount(1);
+            squashAnim2.setRepeatMode(ValueAnimator.REVERSE);
+            squashAnim2.setInterpolator(new DecelerateInterpolator());
+            ValueAnimator stretchAnim1 = ObjectAnimator.ofFloat(newBall, "y", endY,
+                    endY + 25f);
+            stretchAnim1.setDuration(duration/4);
+            stretchAnim1.setRepeatCount(1);
+            stretchAnim1.setInterpolator(new DecelerateInterpolator());
+            stretchAnim1.setRepeatMode(ValueAnimator.REVERSE);
+            ValueAnimator stretchAnim2 = ObjectAnimator.ofFloat(newBall, "height",
+                    newBall.getHeight(), newBall.getHeight() - 25);
+            stretchAnim2.setDuration(duration/4);
+            stretchAnim2.setRepeatCount(1);
+            stretchAnim2.setInterpolator(new DecelerateInterpolator());
+            stretchAnim2.setRepeatMode(ValueAnimator.REVERSE);
+            ValueAnimator bounceBackAnim = ObjectAnimator.ofFloat(newBall, "y", endY,
+                    startY);
+            bounceBackAnim.setDuration(duration);
+            bounceBackAnim.setInterpolator(new DecelerateInterpolator());
+            // Sequence the down/squash&stretch/up animations
+            AnimatorSet bouncer = new AnimatorSet();
+            bouncer.play(bounceAnim).before(squashAnim1);
+            bouncer.play(squashAnim1).with(squashAnim2);
+            bouncer.play(squashAnim1).with(stretchAnim1);
+            bouncer.play(squashAnim1).with(stretchAnim2);
+            bouncer.play(bounceBackAnim).after(stretchAnim2);
+
+            // Fading animation - remove the ball when the animation is done
+            ValueAnimator fadeAnim = ObjectAnimator.ofFloat(newBall, "alpha", 1f, 0f);
+            fadeAnim.setDuration(250);
+            fadeAnim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    balls.remove(((ObjectAnimator)animation).getTarget());
+
+                }
+            });
+
+            // Sequence the two animations to play one after the other
+            AnimatorSet animatorSet = new AnimatorSet();
+            animatorSet.play(bouncer).before(fadeAnim);
+
+            // Start the animation
+            animatorSet.start();
+
+            return true;
+        }
+
+        private ShapeHolder addBall(float x, float y) {
+            OvalShape circle = new OvalShape();
+            circle.resize(50f, 50f);
+            ShapeDrawable drawable = new ShapeDrawable(circle);
+            ShapeHolder shapeHolder = new ShapeHolder(drawable);
+            shapeHolder.setX(x - 25f);
+            shapeHolder.setY(y - 25f);
+            int red = (int)(Math.random() * 255);
+            int green = (int)(Math.random() * 255);
+            int blue = (int)(Math.random() * 255);
+            int color = 0xff000000 | red << 16 | green << 8 | blue;
+            Paint paint = drawable.getPaint(); //new Paint(Paint.ANTI_ALIAS_FLAG);
+            int darkColor = 0xff000000 | red/4 << 16 | green/4 << 8 | blue/4;
+            RadialGradient gradient = new RadialGradient(37.5f, 12.5f,
+                    50f, color, darkColor, Shader.TileMode.CLAMP);
+            paint.setShader(gradient);
+            shapeHolder.setPaint(paint);
+            balls.add(shapeHolder);
+            return shapeHolder;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            for (int i = 0; i < balls.size(); ++i) {
+                ShapeHolder shapeHolder = balls.get(i);
+                canvas.save();
+                canvas.translate(shapeHolder.getX(), shapeHolder.getY());
+                shapeHolder.getShape().draw(canvas);
+                canvas.restore();
+            }
+        }
+    }
+    
 
 }
